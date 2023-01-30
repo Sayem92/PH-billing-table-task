8b6edb2a2949a48066a014b0cb7cba5178e5a106
Sayem92 <sayemmiha123@gmail.com>
2023-01-30 15:51:25 +0600

    add back button

--- src/components/BillingDashboard/BillingTable/BillingTable.js
+++ src/components/BillingDashboard/BillingTable/BillingTable.js
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import Loading from '../../Loading/Loading';
 import { AuthContext } from '../../UseContext/AuthProvider/AuthProvider';
 import AddBillingModal from '../AddBillingModal/AddBillingModal';
@@ -17,6 +18,7 @@ const BillingTable = () => {
 
     const [count, setCount] = useState(0)
     const [page, setPage] = useState(0)
+    const [pagination, setPagination] = useState(true);
 
 
 
@@ -58,6 +60,7 @@ const BillingTable = () => {
 
 
     const handleAddProduct = data => {
+        setPagination(false);
         setLoading(true);
         if (data.filter === "name") {
             const filterName = allBillList?.filter(bill => bill.name === data.inputValue);
@@ -177,36 +180,45 @@ const BillingTable = () => {
                 </div>
             }
 
-            <div className="mt-14">
-                <div className="flex justify-center space-x-3 text-white">
-                    <button title="previous" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md bg-gray-900 border-gray-800">
-                        <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
-                            <polyline points="15 18 9 12 15 6"></polyline>
-                        </svg>
-                    </button>
+            {
+                pagination &&
 
-                    {
-                        [...Array(pages).keys()].map(number =>
-                            <button key={number} onClick={() => setPage(number)}
-                                type="button" className={page === number ? "inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md bg-gray-900 text-white border-violet-400"
+                <div className="mt-14">
+                    <div className="flex justify-center space-x-3 text-white">
+                        <button title="previous" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md bg-gray-900 border-gray-800">
+                            <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
+                                <polyline points="15 18 9 12 15 6"></polyline>
+                            </svg>
+                        </button>
 
-                                    :
+                        {
+                            [...Array(pages).keys()].map(number =>
+                                <button key={number} onClick={() => setPage(number)}
+                                    type="button" className={page === number ? "inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md bg-gray-900 text-white border-violet-400"
 
-                                    '"inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md text-black  border-violet-400"'}
+                                        :
 
-                            >{number + 1}</button>
-                        )
-                    }
+                                        '"inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md text-black  border-violet-400"'}
 
-                    <button title="next" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md bg-gray-900 border-gray-800">
-                        <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
-                            <polyline points="9 18 15 12 9 6"></polyline>
-                        </svg>
-                    </button>
+                                >{number + 1}</button>
+                            )
+                        }
+
+                        <button title="next" type="button" className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md bg-gray-900 border-gray-800">
+                            <svg viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round" className="w-4">
+                                <polyline points="9 18 15 12 9 6"></polyline>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-            </div>
 
-
+            }
+            {
+                !pagination &&
+                <Link to='/home'>
+                    <button className='mt-16 btn bg-yellow-300 hover:bg-yellow-400 border-none text-black '>Home</button>
+                </Link>
+            }
             {
                 modal === 'open' && <AddBillingModal refetch={refetch} setModal={setModal}></AddBillingModal>
             }
